e17d840f81d28b0c89c2de1d38e8b2a1aed7cba9
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-03-09 16:50:10 +0500

    caching with redis

--- src/main/java/com/akobir/cache/entity/Student.java
+++ src/main/java/com/akobir/cache/entity/Student.java
@@ -9,12 +9,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student {
+public class Student implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
